ae7a480620ef97721a0e8824ad794c1edae54fae
Henrique <henriqueazank@gmail.com>
2024-01-26 16:17:55 -0300

    feat: second file

---
+++ BEGGININGS/CHP01/variables.cpp
@@ -0,0 +1,23 @@
+
+#include <cstdio>
+
+// C++ is a strongly typed language
+
+// It means that variables must have a "type",
+// or in other words, a pre-defined collection of behaviors
+
+// Declaring some variable
+int some_variable;
+
+// Initializing some variable with a initial state
+int some_other_variable = 77;
+
+// We can then use these variables in other contexts
+int some_operation = some_other_variable / 11;
+
+// Instantiating a function to
+int main()
+{
+    printf("My integer: %d", some_operation);
+    return 0;
+};
